73d49fe2452fdd47a694fc70916f60103278cdc4
Francesco Luppi <francescoluppi@aol.com>
2020-10-20 09:54:15 +0200

    Esercizi da 1 a 4 di Day6

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -1,2 +1,42 @@
 console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+console.log('Open the console and check if the countries has been loaded')
+//LVL1
+//1
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+let y = 0
+while (y <= 10) {
+    console.log(y)
+    y++
+}
+let x = 0
+do {
+    console.log(x)
+    x++
+} while (x <= 10)
+//2
+for (let i = 10; i >= 10; i--) {
+    console.log(i)
+}
+let y = 10
+while (y >= 0) {
+    console.log(y)
+    y--
+}
+let x = 10
+do {
+    console.log(x)
+    x--
+} while (x >= 0)
+//3
+let n = 5
+for (let i = 0; i < n; i++) {
+    console.log(i)
+}
+//4
+let hash = ''
+for (let i = 0; i < 8; i++) {
+    hash += '#'
+    console.log(hash + '\n')
+}
