f2a1da1ba5eec0b5d92f8f3c7723bcc84de3b47d
Francesco Luppi <francescoluppi@aol.com>
2020-10-29 16:15:05 +0100

    LVL2.2 Update

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -117,12 +117,47 @@ function solveLinEquation(a, b, c) {
     return x
 }
 console.log(solveLinEquation(2, 4, 5))
-//2 - da controllare
+//2
 function solveQuadEquation(a, b, c) {
-    let x = (-b - c) / a
-    return x
+    // program to solve quadratic equation
+    let root1, root2;
+
+    // take input from the user
+    //let a = prompt("Enter the first number: ");
+    //let b = prompt("Enter the second number: ");
+    //let c = prompt("Enter the third number: ");
+
+    // calculate discriminant
+    let discriminant = b * b - 4 * a * c;
+
+    // condition for real and different roots
+    if (discriminant > 0) {
+        root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+        // result
+        console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+    }
+
+    // condition for real and equal roots
+    else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+
+        // result
+        console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+    }
+
+    // if roots are not real
+    else {
+        let realPart = (-b / (2 * a)).toFixed(2);
+        let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
+
+        // result
+        console.log(
+            `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
+        );
+    }
 }
-console.log(solveQuadEquation(2, 4, 5))
 //3
 let arrToBePrinted = [1, 2, 3, 4, 5]
 function printArray(array) {
