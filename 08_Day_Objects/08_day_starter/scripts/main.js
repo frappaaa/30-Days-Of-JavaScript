928a8eca865632e5c3d1c11ba34fab38d1db980a
Francesco Luppi <francescoluppi@aol.com>
2020-11-02 12:52:21 +0100

    DAY8 LVL1.1 - LVL1.5

--- 08_Day_Objects/08_day_starter/scripts/main.js
+++ 08_Day_Objects/08_day_starter/scripts/main.js
@@ -1,2 +1,23 @@
 console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+console.log('Open the console and check if the countries has been loaded')
+//LVL1
+//1
+let dog = {}
+//2
+console.log(dog)
+//3
+dog.name = 'Oliver'
+dog.legs = 4
+dog.color = 'black'
+dog.age = 13
+dog.bark = function () {
+    console.log('woof woof')
+}
+//4
+console.log(Object.values(dog))
+//5
+dog.breed = 'Labrador'
+dog.getDogInfo = function () {
+    console.log(`The dog name is ${this.name}. It has ${this.legs} legs and it is ${this.age} years old. Its color is ${this.color} and the breed is ${this.breed}. It mainly says ${this.bark()} when it sees you.`)
+}
+dog.getDogInfo()
