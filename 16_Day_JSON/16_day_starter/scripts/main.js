fb139f3b620de43675a74dac3e9757e69887cc85
Francesco Luppi <francescoluppi@aol.com>
2020-11-12 13:06:28 +0100

    DAY16 - LVL1.1 - LVL3.2

--- 16_Day_JSON/16_day_starter/scripts/main.js
+++ 16_Day_JSON/16_day_starter/scripts/main.js
@@ -1,2 +1,165 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python']
+let age = 250;
+let isMarried = true
+const student = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayehe',
+    age: 250,
+    isMarried: true,
+    skills: ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python',]
+}
+const txt = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}
+`
+//LVL1
+//1
+console.log(JSON.stringify(skills))
+//2
+console.log(JSON.stringify(age))
+//3
+console.log(JSON.stringify(isMarried))
+//4
+console.log(JSON.stringify(student))
+//LVL2
+//1
+console.log(JSON.stringify(student, ['firstName', 'lastName', 'skills']))
+//LVL3
+//1
+let object = JSON.parse(txt)
+console.log(object)
+//ESEMPIO
+const usersText = `{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+    }`
+
+const usersObj = JSON.parse(usersText, (key, value) => {
+    let newValue = typeof value == 'string' && key != 'email' ? value.toUpperCase() : value
+    return newValue
+})
+console.log(usersObj)
+//2
+let newObj = JSON.parse(txt)
+let sum = 0
+let moreSkills = ''
+let length = Object.keys(newObj).length
+for (let i = 0; i < length; i++) {
+    name = Object.keys(newObj)[i]
+    if (newObj[name].skills.length > sum) {
+        sum = newObj[name].skills.length
+        moreSkills = newObj[name]
+    }
+}
+console.log(moreSkills)
+
