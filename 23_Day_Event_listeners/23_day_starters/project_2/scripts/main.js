d7a6142c94c149cb46befeb5c9ef525962b5b559
Francesco Luppi <francescoluppi@aol.com>
2020-11-18 15:20:07 +0100

    DAY23 - LVL1.2

--- 23_Day_Event_listeners/23_day_starters/project_2/scripts/main.js
+++ 23_Day_Event_listeners/23_day_starters/project_2/scripts/main.js
@@ -1,2 +1,10 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+//LVL1
+//2
+let h1 = document.createElement('h1')
+let p = document.createElement('p')
+document.body.appendChild(h1)
+document.body.appendChild(p)
+document.body.addEventListener('keypress', e => {
+    h1.innerHTML = 'You pressed ' + e.key
+    p.innerHTML = 'The key code is: ' + e.keyCode
+})
