ef74210f231373f35102d60c9fdcae659b8cc064
Francesco Luppi <francescoluppi@aol.com>
2020-11-23 16:34:10 +0100

    DAY29 - LVL1.1

--- 29_Day_Mini_project_animating_characters/29_day_starters/project_1/scripts/main.js
+++ 29_Day_Mini_project_animating_characters/29_day_starters/project_1/scripts/main.js
@@ -1,5 +1,28 @@
 const div = document.querySelector('div')
-function changeBackground() {
-
+div.style.padding = '10px'
+const phrase = document.createElement('p')
+phrase.style.textAlign = 'center'
+div.appendChild(phrase)
+let string = '30 Days of Javascript Challenge 2020'
+let arrayString = string.split('')
+arrayString.forEach(letter => {
+    let l = document.createElement('span')
+    l.textContent = letter
+    l.style.fontSize = '6em'
+    l.style.textTransform = 'uppercase'
+    l.style.fontFamily = 'Nunito'
+    l.style.color = getRandomColor()
+    phrase.appendChild(l)
+})
+let span = document.querySelectorAll('span')
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
-setInterval(changeBackground(), 2000)
+setInterval(() => {
+    div.style.backgroundColor = getRandomColor()
+}, 3000)
