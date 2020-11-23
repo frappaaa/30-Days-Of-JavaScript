1c9e7c748b3d6712b8f02fdeb6e1bfc024eb630c
Francesco Luppi <francescoluppi@aol.com>
2020-11-23 10:55:03 +0100

    DAY26 - LVL1.1

--- 26_Day_World_countries_data_visualization_2/26_day_starter/scripts/main.js
+++ 26_Day_World_countries_data_visualization_2/26_day_starter/scripts/main.js
@@ -1,2 +1,18 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// adding DOM element on the index file
+let searchFilter = document.createElement('input')
+document.body.appendChild(searchFilter)
+let nameCountries = document.createElement('div')
+nameCountries.setAttribute('class', 'names-wrapper')
+document.body.appendChild(nameCountries)
+// adding the event listener on the input element that captures every change
+searchFilter.addEventListener('input', () => {
+    nameCountries.textContent = ''
+    let listName = ''
+    listName = countries.filter(name => name.includes(searchFilter.value))
+    console.log(listName)
+    for (let i = 0; i < listName.length; i++) {
+        let name = document.createElement('p')
+        name.textContent = listName[i]
+        nameCountries.appendChild(name)
+    }
+})
